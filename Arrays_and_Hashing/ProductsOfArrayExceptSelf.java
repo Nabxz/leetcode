f8db2d761f4c21f0410ec600658a706ec71fc0ac
Nabxz <mammanidev@gmail.com>
2025-08-08 02:20:27 -0500

    Solved Product of array except self

--- Arrays_and_Hashing/ProductsOfArrayExceptSelf.java
+++ Arrays_and_Hashing/ProductsOfArrayExceptSelf.java
@@ -5,19 +5,32 @@ package Arrays_and_Hashing;
 * Space Complexity: O(n^2)
 */
 public class ProductsOfArrayExceptSelf {
-    public int[] productExceptSelf(int[] nums) {
-        int[] output = new int[nums.length];
-        for (int i = 0; i < output.length; i++) {
-            output[i] = 1;
-        }
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int[] pref = new int[n];
+        int[] suff = new int[n];
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    output[i] *= nums[j];
-                }
-            }
+        pref[0] = 1;
+        suff[n - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            pref[i] = nums[i - 1] * pref[i - 1];
+            System.out.println(pref[i]);
         }
-        return output;
+        System.out.println();
+        for (int i = n - 2; i >= 0; i--) {
+            suff[i] = nums[i + 1] * suff[i + 1];
+            System.out.println(suff[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            res[i] = pref[i] * suff[i];
+            System.out.println(res[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        productExceptSelf(new int[] { 1, 2, 4, 6 });
     }
 }
