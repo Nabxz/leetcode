23efcab36097e1b572199d07cf0279164eeb5fbb
Nabxz <mammanidev@gmail.com>
2025-09-26 12:38:55 -0500

    Solved TwoIntegerSum

--- Two_Pointers/TwoIntegerSum.java
+++ Two_Pointers/TwoIntegerSum.java
@@ -1,25 +1,25 @@
 /*
-* Time Complexity: O()
-* Space Complexity: O()
-*/
+* Time Complexity: O(n)
+* Space Complexity: O(1)
+ */
 public class TwoIntegerSum {
-  public int[] twoSum(int[] numbers, int target) {
-        
+
+    public int[] twoSum(int[] numbers, int target) {
+
         int leftIndex = 0;
-        int rightIndex = numbers.length;
+        int rightIndex = numbers.length - 1;
         boolean targetFound = false;
 
         while (leftIndex < rightIndex && !targetFound) {
-          if (numbers[left] + numbers[right] == target) {
-            targetFound = true;
-          } else if (numbers[left] + numbers[right] < target) {
-            leftIndex++;
-          } else if (numbers[left] + numbers[right] > target) {
-            rightIndex--;
-          }
+            if (numbers[leftIndex] + numbers[rightIndex] == target) {
+                targetFound = true;
+            } else if (numbers[leftIndex] + numbers[rightIndex] < target) {
+                leftIndex++;
+            } else if (numbers[leftIndex] + numbers[rightIndex] > target) {
+                rightIndex--;
+            }
         }
 
-        return new int[] {leftIndex, rightIndex};
+        return new int[]{leftIndex + 1, rightIndex + 1};
     }
 }
-
