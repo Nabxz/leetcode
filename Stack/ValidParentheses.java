09c3137bc613d23e29317f52abc1a34eabf1a0b3
Nabxz <mammanidev@gmail.com>
2026-01-26 05:22:04 -0600

    Solved Valid parentheses

--- Stack/ValidParentheses.java
+++ Stack/ValidParentheses.java
@@ -1,3 +1,6 @@
+
+import java.util.Stack;
+
 /*
 * Time Complexity: O( )
 * Space Complexity: O( )
@@ -5,13 +8,38 @@
 public class ValidParentheses {
 
     public static boolean isValid(String s) {
-        boolean isValid = true;
+        boolean isValid = s.length() > 1;
+        Stack<Character> stringValidatorStack = new Stack<>();
 
-        return isValid;
+        for (int i = 0; i < s.length(); i++) {
+            char character = s.charAt(i);
+
+            if (character == '(' || character == '[' || character == '{') {
+                stringValidatorStack.push(character);
+            } else if (character == ')') {
+                if (stringValidatorStack.isEmpty() || stringValidatorStack.pop() != '(') {
+                    isValid = false;
+                }
+            } else if (character == ']') {
+                if (stringValidatorStack.isEmpty() || stringValidatorStack.pop() != '[') {
+                    isValid = false;
+                }
+            } else if (character == '}') {
+                if (stringValidatorStack.isEmpty() || stringValidatorStack.pop() != '{') {
+                    isValid = false;
+                }
+            }
+
+        }
+
+        return isValid && stringValidatorStack.isEmpty();
     }
 
     public static void main(String[] args) {
-        System.out.println("Test 1:\nExpected: 9\nResult: " + isValid(""));
-        System.out.println("\nTest 2:\nExpected: 6\nResult: " + isValid(""));
+        System.out.println("Test 1:\nExpected: true\nResult: " + isValid("[]"));
+        System.out.println("\nTest 2:\nExpected: false\nResult: " + isValid("[(])"));
+        System.out.println("\nTest 3:\nExpected: false\nResult: " + isValid("["));
+        System.out.println("\nTest 4:\nExpected: false\nResult: " + isValid("]"));
+        System.out.println("\nTest 5:\nExpected: false\nResult: " + isValid("]]"));
     }
 }
