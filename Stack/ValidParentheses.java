7fee75e9753cf94e6ad73f168b5e2af4d053f4ac
Nabxz <mammanidev@gmail.com>
2026-01-26 05:01:36 -0600

    Started solving Valid parentheses

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,17 @@
+/*
+* Time Complexity: O( )
+* Space Complexity: O( )
+ */
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        boolean isValid = true;
+
+        return isValid;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test 1:\nExpected: 9\nResult: " + isValid(""));
+        System.out.println("\nTest 2:\nExpected: 6\nResult: " + isValid(""));
+    }
+}
