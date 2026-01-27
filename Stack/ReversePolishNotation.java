c53bbae676328d5439414fadcb8363eeba5ce7d7
Nabxz <mammanidev@gmail.com>
2026-01-27 09:03:03 -0600

    Started Solving Reverse Polish Notation

---
+++ Stack/ReversePolishNotation.java
@@ -0,0 +1,16 @@
+/*
+* Time Complexity: O( )
+* Space Complexity: O( )
+ */
+public class ReversePolishNotation {
+
+    public static int evalRPN(String[] tokens) {
+        int value = 0;
+
+        return value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test 1:\nExpected: 5\nResult: " + evalRPN(new String[]{"1", "2", "+", "3", "*", "4", "-"}));
+    }
+}
