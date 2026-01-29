67b2da44ef4d8e85810e186a905937d49f9e1440
Nabxz <mammanidev@gmail.com>
2026-01-29 12:42:38 -0600

    Progress on ReversePolishNotation

--- Stack/ReversePolishNotation.java
+++ Stack/ReversePolishNotation.java
@@ -1,6 +1,6 @@
 /*
-* Time Complexity: O( )
-* Space Complexity: O( )
+* Time Complexity: O(n)
+* Space Complexity: O(n)
  */
 
 import java.util.Stack;
@@ -27,28 +27,26 @@ public class ReversePolishNotation {
 
     public static int evalRPN(String[] tokens) {
         int totalValue = 0;
-        int operand1;
-        int operand2;
-        String operator;
+        boolean firstExpression = true;
 
-        for (int i = tokens.length - 1; i >= 0; i--) {
-            operationStack.push(tokens[i]);
-        }
+        for (String value : tokens) {
 
-        // For the first two operands we extract the the 2 operands ans the operator
-        if (operationStack.size() >= 3) {
-            operand1 = Integer.parseInt(operationStack.pop());
-            operand2 = Integer.parseInt(operationStack.pop());
-            operator = operationStack.pop();
+            if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) {
 
-            totalValue += executeOperation(operand1, operand2, operator);
-
-            // For the remaining operators and operand we extract one operand and one operator
-            while (!operationStack.isEmpty()) {
-                operand1 = Integer.parseInt(operationStack.pop());
-                operator = operationStack.pop();
-                totalValue = executeOperation(totalValue, operand1, operator);
+                if (firstExpression & operationStack.size() >= 2) {
+                    firstExpression = false;
+                    int operand2 = Integer.parseInt(operationStack.pop());
+                    int operand1 = Integer.parseInt(operationStack.pop());
+                    totalValue = executeOperation(operand1, operand2, value);
+                } else if (!firstExpression & operationStack.size() >= 1) {
+                    totalValue = executeOperation(totalValue, Integer.parseInt(operationStack.pop()), value);
+                } else {
+                    // The equation is invalid, but i'm not accounting for that
+                }
+            } else {
+                operationStack.push(value);
             }
+
         }
 
         return totalValue;
@@ -57,5 +55,6 @@ public class ReversePolishNotation {
     public static void main(String[] args) {
         System.out.println("Test 1:\nExpected: 5\nResult: " + evalRPN(new String[]{"1", "2", "+", "3", "*", "4", "-"}));
         System.out.println("Test 2:\nExpected: 6\nResult: " + evalRPN(new String[]{"4", "13", "5", "/", "+"}));
+        System.out.println("Test 3:\nExpected: 22\nResult: " + evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}));
     }
 }
