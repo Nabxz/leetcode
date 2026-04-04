0b8544d71420f156c6632e7e44554c90dedaa850
Nabxz <mammanidev@gmail.com>
2026-04-04 16:28:35 -0500

    Finally finished solving Largest Rectangle Area

--- Stack/LargestRectangleArea.java
+++ Stack/LargestRectangleArea.java
@@ -23,22 +23,28 @@ public class LargestRectangleArea {
             // if it's smaller then pop the last entry and calculate the area it made
             if (heights[index] < validRectangles.peek().height) {
 
-                Rectangle rect = validRectangles.pop();
-                int area = rect.height * (index - 1 - rect.indexStart);
-
-                if (area > largestArea) {
-                    largestArea = area;
+                int rectStartIndex = validRectangles.peek().indexStart;
+                while (!validRectangles.isEmpty() && heights[index] < validRectangles.peek().height) {
+                    Rectangle rect = validRectangles.pop();
+                    int area = rect.height * (index - rect.indexStart);
+                    rectStartIndex = rect.indexStart;
+                    if (area > largestArea) {
+                        largestArea = area;
+                    }
                 }
 
                 // Add the current rectangle
-                validRectangles.push(new Rectangle(rect.indexStart, heights[index]));
+                validRectangles.push(new Rectangle(rectStartIndex, heights[index]));
+            } else {
+                // Add the current rectangle
+                validRectangles.push(new Rectangle(index, heights[index]));
             }
         }
 
         // Check the area of each rectangle left in the stack
         while (!validRectangles.isEmpty()) {
             Rectangle rect = validRectangles.pop();
-            int area = rect.height * (heights.length - 2 - rect.indexStart);
+            int area = rect.height * (heights.length - rect.indexStart);
             if (area > largestArea) {
                 largestArea = area;
             }
@@ -49,6 +55,6 @@ public class LargestRectangleArea {
 
     public static void main(String[] args) {
         System.out.println("Test 1:\nExpected: 8\nResult: " + largestRectangleArea(new int[] { 7, 1, 7, 2, 2, 4 }));
-        System.out.println("Test 2:\nExpected: 7\nResult: " + largestRectangleArea(new int[] { 1, 3, 7 }));
+        System.out.println("\nTest 2:\nExpected: 7\nResult: " + largestRectangleArea(new int[] { 1, 3, 7 }));
     }
 }
