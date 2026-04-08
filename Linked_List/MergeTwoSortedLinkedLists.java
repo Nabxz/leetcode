2d39862e9ef732d17e28d985602ef3893d9ac836
Nabxz <mammanidev@gmail.com>
2026-04-07 19:18:36 -0500

    Started Merge Two Sorted Linked Lists

---
+++ Linked_List/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,26 @@
+/*
+* Time Complexity: O()
+* Space Complexity: O()
+ */
+public class MergeTwoSortedLinkedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
