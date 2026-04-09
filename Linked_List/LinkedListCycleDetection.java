e7a45d25fdf98b3742491eadfd6ec00fe146c504
Nabxz <mammanidev@gmail.com>
2026-04-09 12:23:41 -0500

    Started Linked List Cycle Detection

---
+++ Linked_List/LinkedListCycleDetection.java
@@ -0,0 +1,26 @@
+/*
+* Time Complexity: O()
+* Space Complexity: O()
+ */
+public class LinkedListCycleDetection {
+    public boolean hasCycle(ListNode head) {
+
+    }
+}
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
