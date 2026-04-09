2877e6aa8e61179343619703f55cd28bc095b50f
Nabxz <mammanidev@gmail.com>
2026-04-09 12:32:39 -0500

    Completed Linked List Cycle Detection

--- Linked_List/LinkedListCycleDetection.java
+++ Linked_List/LinkedListCycleDetection.java
@@ -1,14 +1,34 @@
 /*
-* Time Complexity: O()
-* Space Complexity: O()
+* Time Complexity: O(n)
+* Space Complexity: O(1)
  */
 public class LinkedListCycleDetection {
     public boolean hasCycle(ListNode head) {
+        boolean hasCycle = false;
+        ListNode curr = head;
+        ListNode fastCurr = head;
 
+        if (fastCurr != null) {
+            fastCurr = fastCurr.next;
+        }
+
+        while (curr != null && fastCurr != null && !hasCycle) {
+            if (curr == fastCurr) {
+                hasCycle = true;
+            }
+
+            curr = curr.next;
+            fastCurr = fastCurr.next;
+            if (fastCurr != null) {
+                fastCurr = fastCurr.next;
+            }
+        }
+
+        return hasCycle;
     }
 }
 
-public class ListNode {
+class ListNode {
     int val;
     ListNode next;
 
