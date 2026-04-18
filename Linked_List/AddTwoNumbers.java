e0ac398fb10076b5149083c2d8bf127a42ac50fd
Nabxz <mammanidev@gmail.com>
2026-04-17 22:19:07 -0500

    Started Add Two Numbers

---
+++ Linked_List/AddTwoNumbers.java
@@ -0,0 +1,26 @@
+/*
+* Time Complexity: O()
+* Space Complexity: O()
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
