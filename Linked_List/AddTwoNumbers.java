04f6e897506339443759c7a230bf19d8329c9e5f
Nabxz <mammanidev@gmail.com>
2026-04-19 00:18:06 -0500

    Made progress on Add Two Numbers

--- Linked_List/AddTwoNumbers.java
+++ Linked_List/AddTwoNumbers.java
@@ -4,16 +4,50 @@
  */
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
+        ListNode result = null;
+        ListNode resultHead = null;
 
         // Reverse both lists
-        reverseList(l1);
-        reverseList(l2);
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode currValOne = l1;
+        ListNode currValTwo = l2;
 
-        return result;
+        int carryOver = 0;
+        int operand1 = 0;
+        int operand2 = 0;
+        while (currValOne != null || currValTwo != null) {
+
+            // If we still have operand 1
+            if (currValOne != null) {
+                operand1 = currValOne.val;
+                currValOne = currValOne.next;
+            }
+
+            // If we still have operand 2
+            if (currValTwo != null) {
+                operand2 = currValTwo.val;
+                currValTwo = currValTwo.next;
+            }
+
+            // If this is the first solution node
+            if (result == null) {
+                result = new ListNode();
+                resultHead = result;
+            } else {
+                result.next = new ListNode();
+                result = result.next;
+            }
+
+            result.val = (operand1 + operand2 + carryOver) % 10;
+            carryOver = (operand1 + operand2 + carryOver) / 10;
+        }
+
+        resultHead = reverseList(resultHead);
+        return resultHead;
     }
 
-    private void reverseList(ListNode head) {
+    private ListNode reverseList(ListNode head) {
         ListNode curr = head;
         ListNode next = curr.next;
         ListNode prev = null;
@@ -26,6 +60,9 @@ public class AddTwoNumbers {
             // Reverse link
             curr.next = prev;
         }
+
+        head.next = null;
+        return curr;
     }
 }
 
