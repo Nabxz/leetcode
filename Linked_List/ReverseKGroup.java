621021e6f20e5a62667cd9d3eb937da1c875b915
Nabxz <mammanidev@gmail.com>
2026-05-14 21:01:19 -0500

    Started Reverse Reverse Nodes in K-Group

---
+++ Linked_List/ReverseKGroup.java
@@ -0,0 +1,28 @@
+/*
+* Time Complexity: O()
+* Space Complexity: O()
+ */
+class ReverseKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode top = head;
+
+        return top;Re
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
